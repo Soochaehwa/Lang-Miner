ea3711d30e59c5ba84ea09472ecceac164318ce9
Soochaehwa <43947445+Soochaehwa@users.noreply.github.com>
2022-05-30 15:07:27 +0900

    한국어 번역있으면 한국어도 추출

--- core/extract.js
+++ core/extract.js
@@ -41,6 +41,17 @@ export function lang(buffer, modLoader, modVersion) {
     );
 
     if (modId) {
+      const hasKor = zip.readAsText(`assets/${modId}/lang/ko_kr.json`);
+
+      if (hasKor) {
+        zip.extractEntryTo(
+          `assets/${modId}/lang/ko_kr.json`,
+          `${assetsPath}`,
+          true,
+          true
+        );
+      }
+
       zip.extractEntryTo(
         `assets/${modId}/lang/en_us.json`,
         `${assetsPath}`,
