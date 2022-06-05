64d5bbb7e5f3de41ef95284f0faab9cbc7ea2369
Soochaehwa <43947445+Soochaehwa@users.noreply.github.com>
2022-06-05 21:34:24 +0900

    예외 처리

--- utils/noComment.js
+++ utils/noComment.js
@@ -1,14 +1,19 @@
 import stripJsonComments from "strip-json-comments";
 import fs from "fs";
+import log from "./logger.js";
 
 export default function noComment(dir) {
-  const langs = fs.readdirSync(dir);
-  langs.map((lang) => {
-    const json = fs.readFileSync(`${dir}/${lang}`, "utf8");
-    const jsonWithoutComments = JSON.parse(stripJsonComments(json));
-    fs.writeFileSync(
-      `${dir}/${lang}`,
-      JSON.stringify(jsonWithoutComments, null, 2)
-    );
-  });
+  try {
+    const langs = fs.readdirSync(dir);
+    langs.map((lang) => {
+      const json = fs.readFileSync(`${dir}/${lang}`, "utf8");
+      const jsonWithoutComments = JSON.parse(stripJsonComments(json));
+      fs.writeFileSync(
+        `${dir}/${lang}`,
+        JSON.stringify(jsonWithoutComments, null, 2)
+      );
+    });
+  } catch (error) {
+    log.error("JSON 파일 분석 오류");
+  }
 }
